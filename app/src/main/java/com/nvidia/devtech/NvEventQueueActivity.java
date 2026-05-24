b2cae70f5c23c3c492590636301f203238e355bb
kethrnensm <luongluongthihoa55@gmail.com>
2026-05-25 01:48:37 +0700

    Update NvEventQueueActivity.java

--- app/src/main/java/com/nvidia/devtech/NvEventQueueActivity.java
+++ app/src/main/java/com/nvidia/devtech/NvEventQueueActivity.java
@@ -1,24 +1,4 @@
-//----------------------------------------------------------------------------------
-// File:            libs\src\com\nvidia\devtech\NvEventQueueActivity.java
-// Samples Version: Android NVIDIA samples 2 
-// Email:           tegradev@nvidia.com
-// Forum:           http://developer.nvidia.com/tegra/forums/tegra-forums/android-development
-//
-// Copyright 2009-2010 NVIDIA Corporation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-//----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------
 package com.nvidia.devtech;
 
 import android.app.AlertDialog;
@@ -52,6 +32,10 @@ import android.widget.FrameLayout;
 import android.graphics.Color;
 import android.view.ViewGroup;
 import android.view.Gravity;
+import android.widget.TextView;
+import android.os.Handler;
+import android.os.Looper;
+import android.graphics.drawable.GradientDrawable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
